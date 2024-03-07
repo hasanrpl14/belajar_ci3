891084588ec25139f4e3f7a6add5813fb629c033
hasanabdi <4bdisalam.com>
2024-03-07 13:55:34 +0700

    menambahkan post edit delete artikel

--- application/views/admin/post_list.php
+++ application/views/admin/post_list.php
@@ -13,7 +13,7 @@
 			<h1>List Artikel</h1>
 
 			<div class="toolbar">
-				<a href="#" class="button button-primary" role="button">+ Tulis Artikel</a>
+				<a href="<?= site_url('admin/post/new') ?>" class="button button-primary" role="button">+ Tulis Artikel</a>
 			</div>
 
 			<table class="table">
@@ -25,40 +25,76 @@
 					</tr>
 				</thead>
 				<tbody>
+					<?php foreach($articles as $article): ?>
 					<tr>
 						<td>
-							<div>Hello World!</div>
-							<div class="text-gray"><small>12 Jan 2021<small></div>
+							<div><?= $article->title ?></div>
+							<div class="text-gray"><small><?= $article->created_at ?><small></div>
 						</td>
-						<td class="text-center text-gray">Draft</td>
+						<?php if($article->draft === 'true'): ?>
+							<td class="text-center text-gray">Draft</td>
+						<?php else: ?>
+							<td class="text-center text-green">Published</td>
+						<?php endif ?>
 						<td>
 							<div class="action">
-								<a href="#" class="button button-small" role="button">View</a>
-								<a href="#" class="button button-small" role="button">Edit</a>
-								<a href="#" class="button button-small button-danger" role="button">Delete</a>
+								<a href="<?= site_url('article/'.$article->slug) ?>" class="button button-small" target="_blank" role="button">Preview</a>
+								<a href="<?= site_url('admin/post/edit/'.$article->id) ?>" class="button button-small" role="button">Edit</a>
+								<a href="#" 
+									data-delete-url="<?= site_url('admin/post/delete/'.$article->id) ?>" 
+									class="button button-small button-danger" 
+									role="button"
+									onclick="deleteConfirm(this)">Delete</a>
 							</div>
 						</td>
 					</tr>
-					<tr>
-						<td>
-							<div>Hello World!</div>
-							<div class="text-gray"><small>12 Jan 2021<small></div>
-						</td>
-						<td class="text-center text-green">Published</td>
-						<td>
-							<div class="action">
-								<a href="#" class="button button-small" role="button">View</a>
-								<a href="#" class="button button-small" role="button">Edit</a>
-								<a href="#" class="button button-small button-danger" role="button">Delete</a>
-							</div>
-						</td>
-					</tr>
+					<?php endforeach ?>
 				</tbody>
 			</table>
 
 			<?php $this->load->view('admin/_partials/footer.php') ?>
 		</div>
 	</main>
+
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<script>
+		function deleteConfirm(event){
+			Swal.fire({
+				title: 'Delete Confirmation!',
+				text: 'Are you sure to delete the item?',
+				icon: 'warning',
+				showCancelButton: true,
+				cancelButtonText: 'No',
+				confirmButtonText: 'Yes Delete',
+				confirmButtonColor: 'red'
+			}).then(dialog => {
+				if(dialog.isConfirmed){
+					window.location.assign(event.dataset.deleteUrl);
+				}
+			});
+		}
+	</script>
+
+	<?php if($this->session->flashdata('message')): ?>
+		<script>
+			const Toast = Swal.mixin({
+				toast: true,
+				position: 'top-end',
+				showConfirmButton: false,
+				timer: 3000,
+				timerProgressBar: true,
+				didOpen: (toast) => {
+					toast.addEventListener('mouseenter', Swal.stopTimer)
+					toast.addEventListener('mouseleave', Swal.resumeTimer)
+				}
+			})
+
+			Toast.fire({
+				icon: 'success',
+				title: '<?= $this->session->flashdata('message') ?>'
+			})
+		</script>
+	<?php endif ?>
 </body>
 
 </html>
